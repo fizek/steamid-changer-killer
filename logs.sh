9be23d8cc2050ea6812ce4e3f8105b6961742945
fizek <39031566+fizek@users.noreply.github.com>
2019-01-15 23:29:47 +0100

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+#i put this script to cron – it starts every minute
+logpath='/sourcecs/logi'
+# my four servers mentioned below are defined in ~/.rconrc
 for serw in dd2 ffa fun cod
 do
-  rcon -s ${serw} smac_status | grep -a STEAM > /sourcecs/logi/${serw}/${serw}-rcon-logtemp
-  cat /sourcecs/logi/${serw}/${serw}-rcon-logtemp >> /sourcecs/logi/${serw}/${serw}-rcon-log-$(date +%Y-%m-%d-%H)
-  cat /sourcecs/logi/${serw}/${serw}-rcon-logtemp > /var/www/html/${serw}.txt 
+  rcon -s ${serw} smac_status | grep -a STEAM > ${logpath}/${serw}/${serw}-rcon-logtemp
+  cat ${logpath}/${serw}/${serw}-rcon-logtemp >> ${logpath}/${serw}/${serw}-rcon-log-$(date +%Y-%m-%d-%H)
+  cat ${logpath}/${serw}/${serw}-rcon-logtemp > /var/www/html/${serw}.txt 
   date >> /var/www/html/${serw}.txt
 done
-bash /sourcecs/logi/dd2/blokady/blokada.sh
+# below is call for blocking script
+bash ${logpath}/scripts/blokada.sh
